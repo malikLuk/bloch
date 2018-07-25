4683c70e0567e2c913353db65bad31eeea2379c7
Lukmanov.MN <Lukmanov.MN@parma.ru>
2018-07-25 14:25:30 +0500

    article 26

---
+++ src/main/java/ch5/article26/SupportGenerics.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *                                              Поддерживать обобщенные типы.
+ *                                      Посмотреть, как реализовано в AraayList.java
+ *
+ * */
+
+package ch5.article26;
+
+public class SupportGenerics {
+}
